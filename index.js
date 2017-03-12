19c146286e5fa34ed6d0562f1b09516a86db6385
Steven Winston <swinston100@hotmail.com>
2017-03-12 14:48:23 +0200

    takeoff

---
+++ index.js
@@ -0,0 +1,14 @@
+var frontEnd = ['HTML', 'CSS', 'Javascript'];
+var df = 1
+var frontEndLangages = frontEnd;
+
+var Jet = function(color, size) {
+  this.color = color;
+  this.size = size;
+
+  this.takeOff = function() {
+    console.log('lift off!');
+  }
+}
+
+module.exports = Jet;
